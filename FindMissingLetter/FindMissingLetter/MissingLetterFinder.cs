9340bb79e9d82ef2176d1149f46975899c007dd3
urosknapic <uros.knapic@ceneje.si>
2021-11-23 22:02:12 +0100

    add core function

---
+++ FindMissingLetter/FindMissingLetter/MissingLetterFinder.cs
@@ -0,0 +1,26 @@
+﻿namespace FindMissingLetter
+{
+  public static class MissingLetterFinder
+  {
+    public static char FindLetter(char[] array)
+    {
+      if (array != null && array.Length > 1)
+      {
+        int startingValue = array[0];
+        int expectedValue = 0;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+          expectedValue = startingValue + i;
+          var nextLetterDecimal = array[i];
+          if (nextLetterDecimal != expectedValue)
+          {
+            return (char)expectedValue;
+          }
+        }
+      }
+
+      return ' ';
+    }
+  }
+}
