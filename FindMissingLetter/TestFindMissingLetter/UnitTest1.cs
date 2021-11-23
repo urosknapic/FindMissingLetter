457c03332f927da4e0f7ee7966d97c783285c243
urosknapic <uros.knapic@ceneje.si>
2021-11-23 21:26:29 +0100

    Updated test for character array not string array

--- FindMissingLetter/TestFindMissingLetter/UnitTest1.cs
+++ FindMissingLetter/TestFindMissingLetter/UnitTest1.cs
@@ -13,11 +13,67 @@ namespace TestFindMissingLetter
     [Test]
     public void WhenEmptyArray_ReturnEmptyString()
     {
-      string[] emptyArray = new string[] { };
+      var emptyArray = new char[] { };
 
-      string letter = MissingLetterFinder.FindLetter(emptyArray);
+      var letter = MissingLetterFinder.FindLetter(emptyArray);
 
-      Assert.AreEqual(letter, "");
+      Assert.AreEqual(letter, ' ');
+    }
+
+    [Test]
+    public void Finder_WhenOneLetter_ReturnEmptyString()
+    {
+      var array = new char[] { 'a' };
+
+      var letter = MissingLetterFinder.FindLetter(array);
+
+      Assert.AreEqual(letter, ' ');
+    }
+
+    [Test]
+    public void Finder_WhenTwoLettersWithoutMissingLetter_ReturnEmptychar()
+    {
+      char[] array = new char[] { 'a', 'b' };
+
+      Assert.AreEqual(MissingLetterFinder.FindLetter(array), ' ');
+    }
+
+    [Test]
+    public void Finder_WhenTwoLettersWithoutMissingLetterUppercase_ReturnEmptychar()
+    {
+      char[] arrayUppercase = new char[] { 'A', 'B' };
+
+      Assert.AreEqual(MissingLetterFinder.FindLetter(arrayUppercase), ' ');
+    }
+
+    [Test]
+    public void Finder_WhenTwoLettersWithMissingLetterB_ReturnB()
+    {
+      char[] array = new char[] { 'a', 'c' };
+
+      char letter = MissingLetterFinder.FindLetter(array);
+
+      Assert.AreEqual(letter, 'b');
+    }
+
+    [Test]
+    public void Finder_WhenTwoLettersWithMissingLetterBUppercas_ReturnBUppercas()
+    {
+      char[] array = new char[] { 'A', 'C' };
+
+      char letter = MissingLetterFinder.FindLetter(array);
+
+      Assert.AreEqual(letter, 'B');
+    }
+
+    [Test]
+    public void Finder_WhenTwoLettersWithMissingLetterE_ReturnE()
+    {
+      char[] array = new char[] { 'a', 'b', 'c', 'd', 'f' };
+
+      char letter = MissingLetterFinder.FindLetter(array);
+
+      Assert.AreEqual(letter, 'e');
     }
   }
 }
